dc8a2831bfdb7daeb1ef71725ee8712f64b18239
pragnakasthruri <kpragna25@gmail.com>
2022-02-15 23:14:11 +0530

    Added usecase 1 - single player started at 0

---
+++ src/com/bridgelabz/SnakeAndLadderUC1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC1 {
+    private static final int WIN_POS = 100;
+    public static void main(String[] args) {
+        int startPos = 0;
+        System.out.println("Welcome to SnakeAndLadder Game!");
+    }
+}
