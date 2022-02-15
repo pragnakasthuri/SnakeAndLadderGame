311d645eccfba8679db2c47a9da90f9f61ad0498
pragnakasthruri <kpragna25@gmail.com>
2022-02-15 23:22:36 +0530

    Added usecase 2 - user rolls the dice

--- src/com/bridgelabz/SnakeAndLadderUC1.java
+++ src/com/bridgelabz/SnakeAndLadderUC1.java
@@ -1,9 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadderUC1 {
     private static final int WIN_POS = 100;
     public static void main(String[] args) {
         int startPos = 0;
         System.out.println("Welcome to SnakeAndLadder Game!");
+        do {
+            int diceValue = getDiceValue();
+        }while(startPos < WIN_POS);
+
+    }
+
+    public static int getDiceValue() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(7);
+        return randomNumber == 0 ? 1: randomNumber;
     }
 }
