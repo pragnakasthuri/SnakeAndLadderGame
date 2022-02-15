6df0e686a51e3c995e264f9a3dee60e9e96cb96c
pragnakasthruri <kpragna25@gmail.com>
2022-02-15 23:42:44 +0530

    Implemented usecase 3

--- src/com/bridgelabz/SnakeAndLadderUC1.java
+++ src/com/bridgelabz/SnakeAndLadderUC1.java
@@ -6,11 +6,23 @@ public class SnakeAndLadderUC1 {
     private static final int WIN_POS = 100;
     public static void main(String[] args) {
         int startPos = 0;
+        int cnt = 0;
         System.out.println("Welcome to SnakeAndLadder Game!");
         do {
+            cnt++;
             int diceValue = getDiceValue();
+            if (startPos + diceValue > 100) {
+                continue; // no play
+            } else if (diceValue < 3) {
+                startPos-=diceValue;
+                if (startPos < 0) {
+                    startPos = 0;
+                }
+            } else {
+                startPos+=diceValue;
+            }
         }while(startPos < WIN_POS);
-
+        System.out.println("Congratulations ! You won the SnakeAndLadder Game in "+cnt+" steps!");
     }
 
     public static int getDiceValue() {
