b8c9ba3c40b2afaf77028ac95e5f3663f79bc5d1
pragnakasthruri <kpragna25@gmail.com>
2022-02-16 12:56:12 +0530

    Usecase 7 is implemented

--- src/com/bridgelabz/SnakeAndLadderUC1.java
+++ src/com/bridgelabz/SnakeAndLadderUC1.java
@@ -5,28 +5,57 @@ import java.util.Random;
 public class SnakeAndLadderUC1 {
     private static final int WIN_POS = 100;
     public static void main(String[] args) {
-        int startPos = 0;
-        int cnt = 0;
+        int startPosA = 0, startPosB = 0;
+        int diceRollCntA = 0, diceRollCntB = 0;
+        int player = 1;
         System.out.println("Welcome to SnakeAndLadder Game!");
         do {
-            cnt++;
-            int diceValue = getDiceValue();
-            if (startPos + diceValue > 100) {
-                continue; // no play
-            } else if (diceValue < 3) {
-                startPos-=diceValue;
-                if (startPos < 0) {
-                    startPos = 0;
+            int diceValue = rollTheDice();
+            if (player == 1) {
+                diceRollCntA++;
+                if ((startPosA + diceValue) > 100) {
+                    player = 2;
+                    continue; // no play
+                } else if (diceValue < 3) { // considering snake for a dice value < 3
+                    startPosA-=diceValue;
+                    if (startPosA < 0) {
+                        startPosA = 0;
+                    }
+                    player = 2;
+                } else {
+                    startPosA+=diceValue;
+                    player = 1;
+                }
+                System.out.println("Dice Role: "+diceRollCntA+", A Position: "+startPosA);
+                if (startPosA == 100) {
+                    break;
                 }
             } else {
-                startPos+=diceValue;
+                diceRollCntB++;
+
+                if ((startPosB + diceValue) > 100) {
+                    player = 1;
+                    continue; // no play
+                } else if (diceValue < 3) { // considering snake for a dice value < 3
+                    startPosB-=diceValue;
+                    if (startPosB < 0) {
+                        startPosB = 0;
+                    }
+                    player = 1;
+                } else {
+                    startPosB+=diceValue;
+                    player = 2;
+                }
+                System.out.println("Dice Role: "+diceRollCntB+", B Position: "+startPosB);
+                if (startPosB == 100) {
+                    break;
+                }
             }
-            System.out.println("Dice Role: "+cnt+", Position: "+startPos);
-        }while(startPos < WIN_POS);
-        System.out.println("Congratulations ! You won the SnakeAndLadder Game in "+cnt+" steps!");
+        }while(startPosA < WIN_POS || startPosB < WIN_POS);
+        System.out.println("Congratulations "+(startPosA == 100 ? "A" : "B")+"! You won the SnakeAndLadder Game in "+(startPosA == 100 ? diceRollCntA : diceRollCntB)+" steps!");
     }
 
-    public static int getDiceValue() {
+    public static int rollTheDice() {
         Random random = new Random();
         int randomNumber = random.nextInt(7);
         return randomNumber == 0 ? 1: randomNumber;
